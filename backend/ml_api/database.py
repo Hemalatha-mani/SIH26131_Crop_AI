6880df39b89419a9b5cd85b25cfb092d5b293835
Hemalatha-mani <mhemalatharmanivel@gmail.com>
2026-09-15 11:52:26 +0530

    Update database.py

--- backend/ml_api/database.py
+++ backend/ml_api/database.py
@@ -1,7 +1,36 @@
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker, declarative_base
+
+# DATABASE_URL = "postgresql://postgres:postgres123@localhost:5435/cropcare_db"
+
+# engine = create_engine(DATABASE_URL)
+
+# SessionLocal = sessionmaker(
+#     autocommit=False,
+#     autoflush=False,
+#     bind=engine
+# )
+
+# Base = declarative_base()
+
+
+# def get_db():
+#     db = SessionLocal()
+
+#     try:
+#         yield db
+#     finally:
+#         db.close()
+
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:postgres123@localhost:5435/cropcare_db"
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL environment variable is not set")
 
 engine = create_engine(DATABASE_URL)
 
